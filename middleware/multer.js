1667a381373322db131b43d3c54d86aa831c3773
Poornima Batham <pbatham21@gmail.com>
2026-04-25 21:41:27 +0530

    add cdoe

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,8 +1,19 @@
 const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("cloudinary").v2;
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, "uploads/"),
-  filename: (req, file, cb) => cb(null, Date.now() + "-" + file.originalname),
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "grocery-products",
+    allowed_formats: ["jpg", "png", "jpeg"],
+  },
 });
 
 module.exports = multer({ storage });
